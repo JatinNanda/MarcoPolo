59b305737a28f10e62572f347394431539bd7e6e
Devanathan, Jayanth <jdevanathan3@gatech.edu>
2015-09-25 23:43:15 -0400

    Created class for sending SMS. Source files changed: SMSSender.java

--- app/src/main/java/marcopolo/marcopolo/SMSSender.java
+++ app/src/main/java/marcopolo/marcopolo/SMSSender.java
@@ -3,5 +3,12 @@ package marcopolo.marcopolo;
 /**
  * Created by Jay on 9/25/15.
  */
+import android.telephony.SmsManager;
+import java.util.ArrayList;
 public class SMSSender {
+    public ArrayList<String> parseNumbers(String numbers) {
+        SmsManager s = SmsManager.getDefault();
+        return s.divideMessage(numbers);
+    }
+
 }
