4bca7b9d6034c961c5232de76baab4cbf66dcec6
Devanathan, Jayanth <jdevanathan3@gatech.edu>
2015-09-26 04:02:14 -0400

    Works for multiple phone numbers separated by commas.

--- app/src/main/java/marcopolo/marcopolo/SMSActivity.java
+++ app/src/main/java/marcopolo/marcopolo/SMSActivity.java
@@ -23,22 +23,24 @@ public class SMSActivity extends Activity implements OnClickListener{
         findViewById(R.id.sendbutton).setOnClickListener(this);
     }
     public void onClick(View v) {
-        String phoneNumber = ((EditText)
-                findViewById(R.id.editText)).getText().toString();
-        try {
-            SmsManager.getDefault().sendTextMessage(phoneNumber, null, "G45XXXYY"
-                    , null, null);
-        } catch (Exception e) {
-            AlertDialog.Builder alertDialogBuilder = new
-                    AlertDialog.Builder(this);
-            AlertDialog dialog = alertDialogBuilder.create();
+        String[] arr = ((EditText)
+                findViewById(R.id.editText)).getText().toString().split(",");
+        for(String phoneNumber:arr) {
+            try {
+                SmsManager.getDefault().sendTextMessage(phoneNumber, null, "Your Marco Polo Code is:G45XXXYY"
+                        , null, null);
+            } catch (Exception e) {
+                AlertDialog.Builder alertDialogBuilder = new
+                        AlertDialog.Builder(this);
+                AlertDialog dialog = alertDialogBuilder.create();
 
 
-            dialog.setMessage(e.getMessage());
+                dialog.setMessage(e.getMessage());
 
 
-            dialog.show();
+                dialog.show();
 
+            }
         }
     }
     public ArrayList<String> parseNumbers(String numbers) {
